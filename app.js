88a2f6b6b065509281a23c04fe58b13a598fdb81
Natallia Akulenka <58709116+NatalliaA@users.noreply.github.com>
2021-01-25 21:21:50 -0700

    Add connect-mongo

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var express                 = require("express"),
     Exhibit                 = require("./models/exhibit"),
     Comment                 = require ("./models/comment"),
     User                    = require("./models/user"),
+    session                 = require("express-session"),
+    MongoStore              = require('connect-mongo')(session),
     seedDB                  = require("./seeds");
 
 //for hiding api key/secret
@@ -30,6 +32,37 @@ mongoose.connect(dbUrl, {
 })
 .then(()=> console.log ("Connect to DB!"))
 .catch(error => console.log (error.message));
+
+//use Mongo to store a session instead of MemoryStore which is by default
+const secret = process.env.SECRET || 'shouldbeabettersecret';
+const store = new MongoStore({
+    url: dbUrl,
+    secret: secret,
+    //if data is not changed after refreshing a page, no updates needed every time
+    touchAfter: 24*3600 //update only one time in 24h (24*3600 in sec) - lazy update
+});
+//if there are any erors in store
+store.on('error', function(err){
+    console.log("SESSION STORE ERROR: " + err);
+});
+
+const sessionConfig = {
+    store: store,
+    name: 'session',
+    secret: secret,
+    resave: false,
+    saveUninitialized: true,    
+    cookie: {
+        //expires in a week from now (mlsec. sec, min, houres, days)
+        expires: Date.now() + 1000*60*60*24*7,
+        //security for production over https request, won't work with localhost and breaks e.g. login
+        //secure: true,
+        maxAge: 1000*60*60*24*7,
+        //extra security, that user doesnt modify through browser or server
+        httpOnly: true
+    }    
+  };
+app.use(session(sessionConfig));
    
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
